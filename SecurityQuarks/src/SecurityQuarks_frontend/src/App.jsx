35ed1155f067353edcbcbc72d22a942d25fbe039
PaulTJ30 <pultj30@gmail.com>
2024-10-18 13:02:34 -0600

    First commit

---
+++ SecurityQuarks/src/SecurityQuarks_frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HackForm, HackTable } from './HackForm'; // Importa ambos componentes
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+export const HackContext = React.createContext();
+
+const App = () => {
+  const [formData, setFormData] = useState({
+    Q1: "",
+    Q2: "",
+    Q3: "",
+    Q4: "",
+    Q5: "",
+    Q6: ""
+  });
+
+  return (
+    <HackContext.Provider value={{ formData, setFormData }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HackForm />} /> {/* Página de formulario */}
+          <Route path="/table" element={<HackTable />} /> {/* Página de tabla */}
+        </Routes>
+      </Router>
+    </HackContext.Provider>
+  );
+};
+
+export default App;
+
+App
