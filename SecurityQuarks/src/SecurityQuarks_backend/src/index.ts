82bd7ff28bf246773c3643bf0e0c6c66a70aa573
DanielArellano31 <160263309+DanielArellano31@users.noreply.github.com>
2024-10-18 20:35:52 -0600

    Delete SecurityQuarks directory

--- SecurityQuarks/src/SecurityQuarks_backend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Server } from "azle";
-import express from "express";
-import cors from "cors";
-
-export default Server(() => {
-    const app = express();
-    app.use(cors());
-    return app.listen()
-})
